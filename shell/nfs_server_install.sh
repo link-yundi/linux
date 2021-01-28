9bc9173209a794c544c1b19938e0b669934760ec
yundi <yundi.xxii@outlook.com>
2021-01-28 23:33:51 +0800

    nfs-server

---
+++ shell/nfs_server_install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# 停止并禁用防火墙
+systemctl stop firewalld
+systemctl disable firewalld
+
+# 关闭并禁用selinux
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=disabled/' /etc/selinux/config
+getenforce
+
+# 安装nfs-utils和rpcbind
+yum install -y nfs-utils rpcbind
+
+# 创建存储文件夹
+mkdir /nfs
+chown -R root.root /nfs
+
+# 配置nfs
+cat << EOF >> /etc/exports
+/nfs */24(rw,async,no_root_squash)
+EOF
+
+# 设置开机启动并启动
+systemctl restart rpcbind
+systemctl enable rpcbind
+systemctl enable nfs
+systemctl restart nfs
+
+# 查看是否有可用的 nfs地址
+showmount -e $(curl ip.sb)
